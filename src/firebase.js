6e829fb6eba5de34eed5bbff46863d34b7a2a5e6
Billy Rodríguez Morales <42099402+BillyVector117@users.noreply.github.com>
2021-01-10 09:04:28 -0600

    Update firebase.js
    Your firebase account here

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-xIwWjGONfH0w42ef_3RMEQnG_HJ-ua8",
-  authDomain: "chat-project-ed2e6.firebaseapp.com",
-  projectId: "chat-project-ed2e6",
-  storageBucket: "chat-project-ed2e6.appspot.com",
-  messagingSenderId: "673418888997",
-  appId: "1:673418888997:web:c04f67061f3c2f9b797ccc",
+  apiKey: "XXX-XXX-XXX",
+  authDomain: "XXX-XXX-XXX",
+  projectId: "XXX-XXX-XXX",
+  storageBucket: "XXX-XXX-XXX",
+  messagingSenderId: "XXX-XXX-XXX",
+  appId: "XXX-XXX-XXX",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
